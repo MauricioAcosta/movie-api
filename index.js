e334dbeaf9846cfb873623ee9f31142b29a2a3ab
MauricioAcosta <amacostapulido@gmail.com>
2020-09-30 17:10:05 -0500

    not found

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const moviesApi = require('./routes/movies.js');
 
 const {
   logErrors,
+  wrapErrors,
   errorHandler
 } = require('./utils/middleware/errorHandlers.js');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
-
+//rutas
 moviesApi(app);
 
+//capturar erro 404 siempre va al final de las rutas
+app.use(notFoundHandler);
+
+//Manejadores de errores
 app.use(logErrors);
+app.use(wrapErrors)
 app.use(errorHandler);
 
 app.listen(config.port, function() {
